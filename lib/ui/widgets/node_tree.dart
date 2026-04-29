3ef1a69a4f05995c18b91d4c1b60f82a106df65b
paul-in <paul-in@mailo.com>
2026-04-29 16:54:58 +0200

    feat: auto expand selected node parents in NodeTree

--- lib/ui/widgets/node_tree.dart
+++ lib/ui/widgets/node_tree.dart
@@ -3,11 +3,12 @@ import "package:flutter/foundation.dart";
 import "package:mycelium/core/stores/node_store.dart";
 import "package:mycelium/data/models/node.dart";
 import 'package:provider/provider.dart';
+import 'package:collection/collection.dart';
 
 class NodeTree extends StatefulWidget {
   final List<Node> nodes;
-
   final void Function(int id)? clickCallback;
+  final bool Function(Node node)? isSpore;
 
   const NodeTree({
     super.key,
@@ -15,8 +16,6 @@ class NodeTree extends StatefulWidget {
     this.isSpore,
     this.clickCallback,
   });
-
-  final bool Function(Node node)? isSpore;
 
   @override
   State<NodeTree> createState() => _NodeTreeState();
@@ -26,127 +25,145 @@ class _NodeTreeState extends State<NodeTree> {
   final TreeSliverController controller = TreeSliverController();
 
   List<TreeSliverNode<Node>> _tree = [];
+  Node? selectedNode;
 
   @override
   void initState() {
     super.initState();
-    _tree = buildTree(widget.nodes);
+    final store = context.read<NodeStore>();
+    selectedNode = store.currentNode;
+    _tree = buildTree(widget.nodes, selectedNode: selectedNode);
   }
+
 
   @override
   void didUpdateWidget(covariant NodeTree oldWidget) {
     super.didUpdateWidget(oldWidget);
-
     if (!listEquals(oldWidget.nodes, widget.nodes)) {
       setState(() {
-        _tree = buildTree(widget.nodes);
+        _tree = buildTree(widget.nodes, selectedNode: selectedNode);
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final selectedNode = context.select<NodeStore, Node?>((s) => s.currentNode);
-    return Scaffold(
-      body: CustomScrollView(
+    return CustomScrollView(
         slivers: [
           TreeSliver(
             tree: _tree,
             controller: controller,
-            treeNodeBuilder:
-                (
-                  BuildContext context,
-                  TreeSliverNode<dynamic> node,
-                  AnimationStyle animationStyle,
-                ) {
-                  final Node typedNode = node.content as Node;
-                  final isSelected = selectedNode?.id == typedNode.id;
-                  final hasChildren = node.children.isNotEmpty;
-                  final isSporeNode = widget.isSpore?.call(typedNode) ?? false;
+            treeNodeBuilder: (
+              BuildContext context,
+              TreeSliverNode<dynamic> node,
+              AnimationStyle animationStyle,
+            ) {
+              final Node typedNode = node.content as Node;
+              final isSelected = selectedNode?.id == typedNode.id;
+              final hasChildren = node.children.isNotEmpty;
+              final isSporeNode = widget.isSpore?.call(typedNode) ?? false;
 
-                  return Row(
-                    children: [
-                      SizedBox(
-                        width: 40,
-                        child: hasChildren
-                            ? InkWell(
-                                borderRadius: BorderRadius.circular(20),
-                                onTap: () {
-                                  setState(() {
-                                    controller.toggleNode(node);
-                                  });
-                                },
-                                child: Center(
-                                  child: AnimatedRotation(
-                                    turns: node.isExpanded ? 0.25 : 0,
-                                    duration: const Duration(milliseconds: 200),
-                                    child: const Icon(
-                                      Icons.chevron_right,
-                                      size: 20,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            : const SizedBox(),
-                      ),
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            widget.clickCallback?.call(typedNode.id);
-                            FocusManager.instance.primaryFocus?.unfocus();
-                            Navigator.pop(context);
-                          },
-                          child: Container(
-                            alignment: Alignment.centerLeft,
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            decoration: BoxDecoration(
-                              color: isSelected
-                                  ? Theme.of(
-                                      context,
-                                    ).colorScheme.primary.withValues(alpha: 0.2)
-                                  : Colors.transparent,
-                              border: Border(
-                                left: BorderSide(
-                                  color: isSporeNode
-                                      ? Theme.of(context).colorScheme.primary
-                                      : Colors.transparent,
-                                  width: 2,
+              return Row(
+                children: [
+                  SizedBox(
+                    width: 40,
+                    child: hasChildren
+                        ? InkWell(
+                            borderRadius: BorderRadius.circular(20),
+                            onTap: () {
+                              setState(() {
+                                controller.toggleNode(node);
+                              });
+                            },
+                            child: Center(
+                              child: AnimatedRotation(
+                                turns: node.isExpanded ? 0.25 : 0,
+                                duration: const Duration(milliseconds: 200),
+                                child: const Icon(
+                                  Icons.chevron_right,
+                                  size: 20,
                                 ),
                               ),
-                              borderRadius: BorderRadius.circular(8),
                             ),
-                            child: Text(
-                              "${typedNode.content?['0']}",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
+                          )
+                        : const SizedBox(),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        widget.clickCallback?.call(typedNode.id);
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        decoration: BoxDecoration(
+                          color: isSelected
+                              ? Theme.of(context)
+                                  .colorScheme
+                                  .primary
+                                  .withValues(alpha: 0.2)
+                              : Colors.transparent,
+                          border: Border(
+                            left: BorderSide(
+                              color: isSporeNode
+                                  ? Theme.of(context).colorScheme.primary
+                                  : Colors.transparent,
+                              width: 2,
                             ),
                           ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          "${typedNode.content?['0']}",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
                       ),
-                    ],
-                  );
-                },
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ],
-      ),
     );
   }
 }
 
-List<TreeSliverNode<Node>> buildTree(List<Node> nodes) {
+List<TreeSliverNode<Node>> buildTree(
+  List<Node> nodes, {
+  Node? selectedNode,
+}) {
+  // ClaudeAI
   final Map<int?, List<Node>> grouped = {};
 
   for (final node in nodes) {
     grouped.putIfAbsent(node.parentId, () => []).add(node);
   }
 
+  final Set<int> ancestorIds = {};
+  if (selectedNode != null) {
+    Node? current =
+        nodes.firstWhereOrNull((n) => n.id == selectedNode.id);
+    while (current?.parentId != null) {
+      ancestorIds.add(current!.parentId!);
+      current = nodes.firstWhereOrNull((n) => n.id == current!.parentId);
+    }
+  }
+
   TreeSliverNode<Node> build(Node node) {
     final children = grouped[node.id] ?? [];
+    final shouldExpand = ancestorIds.contains(node.id);
 
-    return TreeSliverNode<Node>(node, children: children.map(build).toList());
+    return TreeSliverNode<Node>(
+      node,
+      expanded: shouldExpand,
+      children: children.map(build).toList(),
+    );
   }
 
   final roots = grouped[null] ?? [];
-
   return roots.map(build).toList();
 }
