c1f13dbaaf6860c808f3cc501c1a0574ec12e99d
paul-in <paul-in@mailo.com>
2026-05-20 19:31:58 +0200

    feat: can add custom leading to appbar

--- lib/ui/widgets/app_bar.dart
+++ lib/ui/widgets/app_bar.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String titleText;
   final List<Widget>? actions;
+  final Widget? leading;
 
-  const MyAppBar({super.key, required this.titleText, this.actions});
+  const MyAppBar({super.key, required this.titleText, this.actions, this.leading});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: leading,
+      automaticallyImplyLeading: leading == null,
       centerTitle: true,
       title: Text(titleText),
       backgroundColor: Theme.of(context).colorScheme.primary,
