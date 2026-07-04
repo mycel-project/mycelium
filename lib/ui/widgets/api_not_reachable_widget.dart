ae3e90d46d4d19178423a3a9f2f4a5c7ea7b3908
paul-in <paul-in@mailo.com>
2026-07-04 03:06:32 +0200

    fix: Simplify api_not_reachble_widget

--- lib/ui/widgets/api_not_reachable_widget.dart
+++ lib/ui/widgets/api_not_reachable_widget.dart
@@ -6,46 +6,25 @@ class ApiNotReachableWidget extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(32),
-              child: Text.rich(
-                TextSpan(
-                  style: const TextStyle(),
-                  children: [
-                    const TextSpan(
-                      text:
-                          "Mycel API is not reachable.\n\n"
-                          "You can retry by tapping the ",
-                    ),
-                    WidgetSpan(
-                      alignment: PlaceholderAlignment.middle,
-                      child: Icon(Icons.error, size: 18, color: Colors.red),
-                    ),
-                    const TextSpan(
-                      text:
-                          " icon in the app bar, or view more information in the API configuration screen.",
-                    ),
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(32),
+            child: Text(
+              "Mycel API must be configured.",
+              textAlign: TextAlign.center,
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => ApiConfigPage()),
-                );
-              },
-              child: Text("API configuration"),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => ApiConfigPage()),
             ),
-          ],
-        ),
+            child: const Text("Open API configuration"),
+          ),
+        ],
       ),
     );
   }
